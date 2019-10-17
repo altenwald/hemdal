79107d56cd7a0b21fadd0c1deb29549ed3dd7bd3
Manuel Rubio <manuel@altenwald.com>
2019-10-17 19:53:38 +0200

    improve release a bit more

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ ERLANG_VSN=22.0
 ELIXIR_VSN=1.8.2
 PROJECT=hemdal
 
-./build_package.sh
-#docker run -it --rm \
-#           -v $(pwd):/${PROJECT} \
-#           -w /${PROJECT} \
-#           altenwald/phoenix:otp${ERLANG_VSN}_ex${ELIXIR_VSN} ./build_package.sh
+docker run -it --rm \
+           -v $(pwd):/${PROJECT} \
+           -w /${PROJECT} \
+           altenwald/phoenix:otp${ERLANG_VSN}_ex${ELIXIR_VSN} ./build_package.sh
--- build_package.sh
+++ build_package.sh
@@ -3,4 +3,9 @@
 export MIX_ENV=prod
 
 mix local.hex --force
-mix do deps.get, compile, distillery.release --upgrade --env=prod
+mix do deps.get, compile
+cd assets
+npm i
+node node_modules/webpack/bin/webpack.js --mode production
+cd ..
+mix do phx.digest, release --upgrade --env=prod
